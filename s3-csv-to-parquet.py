36b63e2b11388f8866ea99aed4a5658a57e87239
faizan <faizan.r@tmac.ai>
2023-06-05 20:25:37 +0100

    update for legibility and clarity

--- s3-csv-to-parquet.py
+++ s3-csv-to-parquet.py
@@ -1,3 +1,12 @@
+# This Python script is used for converting CSV files stored in AWS S3 into Parquet files. 
+# The script first reads CSV files from the specified directories, converts the CSV data 
+# into a Pandas DataFrame, and then writes the DataFrame as a Parquet file back to S3.
+# The script operates in a chunked manner, meaning it processes a set amount of data at a time, 
+# which is beneficial for large files that cannot be loaded into memory all at once. 
+# The script uses the PyArrow library for writing data in the Parquet format, which is a 
+# columnar storage file format optimized for use with big data processing frameworks.
+
+# Import necessary libraries
 from transform_s3_csv_to_parquet_config import *
 import s3fs
 import pyarrow.parquet as pq
@@ -5,82 +14,99 @@ import pyarrow as pa
 import pandas as pd
 import sys
 
+# Initialize S3 file system
 s3 = s3fs.S3FileSystem(anon=False)
 
-if year_to_process in ["ALL"] + [str(Y) for Y in range(2000, 2101)]:
-    if year_to_process == "ALL":
-        csv_dirs = sorted(set(['/'.join(d.split('/')[2:5]).split('_')[0] for d in s3.glob(f"{indir}/*/*/*")]))
-    else:
-        csv_dirs = sorted(set(['/'.join(d.split('/')[2:5]).split('_')[0] for d in s3.glob(f"{indir}/*/*/{year_to_process}_*")]))
-    print(f"got {len(csv_dirs)} to process for {year_to_process} year(s)")
-else:
-    print(f"{year_to_process} not 'ALL' or a YYYY value")
+# Define data types and column names for CSV data
+column_data_types = {"date": int, "type": str, "price": float, "amount": float}
+column_names = ["date", "type", "price", "amount"]
 
-dtype = {
-    "date": int,
-    "type": str,
-    "price": float,
-    "amount": float,
-}
-usecols = [0, 1, 2, 3]
-names = ["date", "type", "price", "amount"]
-
-
-def do_work(work_dir, work_data, p):
-    def my_filename(keys):
-        return (
-            "_".join(["kaiko-order_book"] + list(keys) + ["part_{i}_" + str(p).zfill(3)])
-            + ".parquet"
-        )
-    work_data["date"] = pd.to_datetime(work_data["date"], unit="ms")
-    work_data = work_data.assign(
-        **{"exchange": exchange.replace(" ", ""), "symbol": symbol, "year": year}
+# Function to process a set amount of data (a "chunk") and save it as a Parquet file
+def process_and_save_as_parquet(work_directory, data_to_process, partition_index):
+    # Helper function to create a filename for a Parquet file
+    def create_filename(keys):
+        return "_".join(["kaiko-order_book"] + list(keys) + ["part_{i}_" + str(partition_index).zfill(3)]) + ".parquet"
+    
+    # Convert date column from integer to datetime format
+    data_to_process["date"] = pd.to_datetime(data_to_process["date"], unit="ms")
+    
+    # Add additional columns to the DataFrame
+    data_to_process = data_to_process.assign(
+        exchange=exchange.replace(" ", ""),
+        symbol=symbol,
+        year=year
     ).reset_index(drop=True)
+    
+    # Write the DataFrame as a Parquet file
     pq.write_to_dataset(
-        pa.Table.from_pandas(work_data),
+        pa.Table.from_pandas(data_to_process),
         root_path=outdir,
         partition_cols=["exchange", "symbol", "year"],
-        basename_template=my_filename([exchange.replace(" ", ""), symbol, year]),
+        basename_template=create_filename([exchange.replace(" ", ""), symbol, year]),
         filesystem=s3,
         coerce_timestamps="us",
         use_deprecated_int96_timestamps=True,
         compression="ZSTD",
         compression_level=19
     )
-    return
 
+# Function to process CSV directories based on the provided year
+def process_csv_directories(year):
+    # Check the year value and return the directories to be processed
+    if year not in ["ALL"] + [str(Y) for Y in range(2000, 2101)]:
+        print(f"{year} not 'ALL' or a YYYY value")
+        return
 
-for work_dir in csv_dirs:
-    exchange, symbol, year = work_dir.split("/")
-    print(f"{[exchange, symbol, year]} :: ", end="")
-    work_csvs = sorted(["s3://" + d for d in s3.glob(indir + "/" + work_dir + "*/*")])
-    if len(work_csvs) < 0:
+    # Get a list of directories to process
+    directories_to_process = sorted(set(['/'.join(directory.split('/')[2:5]).split('_')[0] for directory in s3.glob(f"{indir}/*/*/{year}_*")])) if year != "ALL" else sorted(set(['/'.join(directory.split('/')[2:5]).split('_')[0] for directory in s3.glob(f"{indir}/*/*/*")]))
+    print(f"got {len(directories_to_process)} to process for {year} year(s)")
+
+    # Process each directory
+    for directory in directories_to_process:
+        process_directory(directory)
+
+# Function to process a single directory
+def process_directory(directory):
+    # Split the directory string into individual parts
+    exchange, symbol, year = directory.split("/")
+
+    # Get a list of CSV files in the directory
+    csv_files = sorted(["s3://" + file for file in s3.glob(indir + "/" + directory + "*/*")])
+    if not csv_files:
         print('skip, no data')
-        continue
-    work_data = pd.DataFrame()
-    p = 1
-    for work_csv in work_csvs:
-        for i, chunk in enumerate(
+        return
+
+    # Initialize an empty DataFrame to store CSV data
+    data_to_process = pd.DataFrame()
+    partition_index = 1
+
+    # Process each CSV file in a chunked manner
+    for csv_file in csv_files:
+        for i, data_chunk in enumerate(
             pd.read_csv(
-                work_csv,
+                csv_file,
                 index_col=False,
-                dtype=dtype,
-                usecols=usecols,
+                dtype=column_data_types,
+                usecols=column_names,
                 float_precision="high",
                 chunksize=chunksize,
                 low_memory=False,
             )
         ):
-            work_data = pd.concat([work_data, chunk])
-            if len(work_data) < filesize:
-                continue
-            do_work(work_dir, work_data, p)
-            work_data = pd.DataFrame()
-            p += 1
+            # Append the chunk to the DataFrame
+            data_to_process = pd.concat([data_to_process, data_chunk])
+            
+            # If the DataFrame is large enough, process it and save it as a Parquet file
+            if len(data_to_process) >= filesize:
+                process_and_save_as_parquet(directory, data_to_process, partition_index)
+                data_to_process = pd.DataFrame()
+                partition_index += 1
 
-    if len(work_data) > 0:
-        do_work(work_dir, work_data, p)
-        work_data = pd.DataFrame()
-    print(f"OK")
+    # If there is any remaining data in the DataFrame, process it and save it as a Parquet file
+    if len(data_to_process) > 0:
+        process_and_save_as_parquet(directory, data_to_process, partition_index)
 
-print("all done")
+    print(f"Processed: {exchange}, {symbol}, {year}")
+
+# Call function to process directories for a specific year
+process_csv_directories(year_to_process)
